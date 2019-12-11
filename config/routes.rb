6e8c29b358ecb03cb775f5b967092442e47a2b5b
Elvis Ozoria <elvis.ozoria@gmail.com>
2019-12-10 22:52:57 -0400

    finishing touches in declaring the usercontroller functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   # root "home#index"
-  root "tasks#new"
+  root "home#index"
   devise_for :users 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
