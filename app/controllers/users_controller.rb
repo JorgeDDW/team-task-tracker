5b98ba79a7ca6a882b49b8095a5b76509e4c61a7
Elvis Ozoria <elvis.ozoria@gmail.com>
2019-12-10 22:57:55 -0400

    Hotfix v2 (#8)
    * testing if changing the home page fixes the problem
    
    * seting precompile assets to true
    
    * adding a basic constructors to user controller
    
    * finishing touches in declaring the usercontroller functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,24 +6,10 @@ class UsersController < ApplicationController
   
   
     def show
-    #   @user  = User.find(params[:id])    
     end
   
     def update
-    #   @user = current_user
-    #   if @user.update_attributes(current_user_params)
-    #     flash[:alert] = "Guardado"
-    #   else
-    #     flash[:alert] = "No se ha podido actualizar..."
-    #   end
-    #     redirect_to dashboard_path
     end
-  
-    # private
-    # def current_user_params
-    #     params.require(:user).permit(:from, :about, :status, :language, :avatar, :user_id)
-  
-    # end
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :tasks
   # root "home#index"
   root "tasks#new"
+
   devise_for :users 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
