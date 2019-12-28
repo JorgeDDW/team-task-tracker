048ad5d3848317334112f53368ae28589024d1c0
Jorge Dominguez <jorge@innova-code.com>
2019-12-28 00:05:40 -0400

    adding tag system, need to be improved

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,16 @@
 class Task < ApplicationRecord
+    has_many :taggings
+    has_many :tags, through: :taggings
+    
+    def all_tags=(names)
+        self.tags= names.split(",").map do |name|
+            Tag.where(name: name.strip).first_or_create!
+        end
+    end
+
+    def all_tags
+        self.tags.map(&:name).join(", ")
+    end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_202440) do
+ActiveRecord::Schema.define(version: 2019_12_28_035913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["task_id"], name: "index_taggings_on_task_id"
+  end
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
@@ -41,8 +50,8 @@ ActiveRecord::Schema.define(version: 2019_12_05_202440) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "company", default: "Freelance", null: false
-    t.string "full_name", null: false
+    t.string "company", default: "", null: false
+    t.string "full_name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -57,4 +66,6 @@ ActiveRecord::Schema.define(version: 2019_12_05_202440) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "taggings", "tasks"
 end
