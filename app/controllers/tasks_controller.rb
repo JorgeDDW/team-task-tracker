04df05ae2e6b07a7350bbc3614552dd267f3e37f
Jorge Dominguez <jorge@innova-code.com>
2019-12-31 16:40:32 -0400

    add icon to save button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :assigned, :content, :due_date, :tag, :status, :creator, :time)
+      params.require(:task).permit(:name, :assigned, :content, :due_date, :status, :creator, :time)
     end
 end
